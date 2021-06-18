8f0ce2480c8c4820f3ebe1a13a1769f480330f28
Kiichi Tachibana <bodega1123@gmail.com>
2021-06-18 21:29:42 +0900

    Add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Head from 'next/head'
 import {
   Button,
   TextField,
@@ -57,54 +58,60 @@ const Home: React.FC = () => {
   // }, [lang, text])
 
   return (
-    <Box maxWidth="500px" marginX="auto" mt="30px">
-      <Box mb="15px">
-        <TextField
-          multiline
-          rows={4}
-          variant="outlined"
-          placeholder="日本語または英語の文章を入力してください。"
-          fullWidth
-          value={text}
-          onChange={handleChangeText}
-        ></TextField>
+    <>
+      <Head>
+        <title>Next Text 2 Speech</title>
+        <meta property="og:title" content="Next Text 2 Speech" key="title" />
+      </Head>
+      <Box maxWidth="500px" marginX="auto" mt="30px">
+        <Box mb="15px">
+          <TextField
+            multiline
+            rows={4}
+            variant="outlined"
+            placeholder="日本語または英語の文章を入力してください。"
+            fullWidth
+            value={text}
+            onChange={handleChangeText}
+          ></TextField>
+        </Box>
+        <Box mb="15px">
+          <InputLabel shrink>読み上げに使用する言語</InputLabel>
+          <Select fullWidth value={lang} onChange={handleChangeLang}>
+            <MenuItem value="ja-JP">日本語</MenuItem>
+            <MenuItem value="en-US">英語</MenuItem>
+          </Select>
+        </Box>
+        <Box mb="15px">
+          <InputLabel shrink>読み上げに使用する声色</InputLabel>
+          <Select
+            fullWidth
+            value={voice}
+            onChange={handleChangeVoice}
+            displayEmpty
+          >
+            <MenuItem value="">デフォルト</MenuItem>
+            {speechSynthesisVoices.map((voice) => (
+              <MenuItem
+                key={voice.name}
+                value={voice.name}
+              >{`${voice.name} (${voice.lang})`}</MenuItem>
+            ))}
+          </Select>
+        </Box>
+        <Box>
+          <Button
+            variant="contained"
+            disableElevation
+            color="primary"
+            fullWidth
+            onClick={handleSpeech}
+          >
+            文章を読み上げる
+          </Button>
+        </Box>
       </Box>
-      <Box mb="15px">
-        <InputLabel shrink>読み上げに使用する言語</InputLabel>
-        <Select fullWidth value={lang} onChange={handleChangeLang}>
-          <MenuItem value="ja-JP">日本語</MenuItem>
-          <MenuItem value="en-US">英語</MenuItem>
-        </Select>
-      </Box>
-      <Box mb="15px">
-        <InputLabel shrink>読み上げに使用する声色</InputLabel>
-        <Select
-          fullWidth
-          value={voice}
-          onChange={handleChangeVoice}
-          displayEmpty
-        >
-          <MenuItem value="">デフォルト</MenuItem>
-          {speechSynthesisVoices.map((voice) => (
-            <MenuItem
-              key={voice.name}
-              value={voice.name}
-            >{`${voice.name} (${voice.lang})`}</MenuItem>
-          ))}
-        </Select>
-      </Box>
-      <Box>
-        <Button
-          variant="contained"
-          disableElevation
-          color="primary"
-          fullWidth
-          onClick={handleSpeech}
-        >
-          文章を読み上げる
-        </Button>
-      </Box>
-    </Box>
+    </>
   )
 }
 
